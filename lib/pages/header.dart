30eb5c3dd7a783559941cc6f49ccb8bfa0b51227
Paulo Filho <pcf.feat@gmail.com>
2024-03-05 15:36:11 -0300

    feat: add header

---
+++ lib/pages/header.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 150,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/plus.png', width: 28),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Image.asset('assets/images/heart.png', width: 28),
+                ),
+                Image.asset('assets/images/chat.png', width: 28),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_academiadoflutter_foundations_challenge/pages/header.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Insta-Like'),
-        backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.8),
-        foregroundColor: Colors.white,
-      ),
-      body: Container(),
+    return Column(
+      children: [
+        Header(),
+      ],
     );
   }
 }
